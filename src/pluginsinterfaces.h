4dcf7c5c0a73ff3d95f330327bb80cc327b4cc54
jlbiord <jlbiord@31becf8c-a71e-0410-8e62-47ffcaada4dd>
2008-05-16 11:51:21 +0000

    - The plugin AStyle use now the same codec of the textedit.

--- src/pluginsinterfaces.h
+++ src/pluginsinterfaces.h
@@ -7,7 +7,7 @@ public:
 	enum Action{ReplaceSelection, ReplaceAll, Append};
     virtual ~TextEditInterface() {}
 	virtual QString menuName() const = 0;
-    virtual QString text(QString text, QString selectedText) = 0;
+    virtual QString text(QString text, QString selectedText, QTextCodec *codec) = 0;
     virtual TextEditInterface::Action action() const = 0;
     virtual bool hasConfigDialog() const = 0;
     virtual void config() = 0;
