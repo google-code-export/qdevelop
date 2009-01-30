899847347f0223676095f92be3128b736ae76766
Lord.Divius <Lord.Divius@31becf8c-a71e-0410-8e62-47ffcaada4dd>
2009-01-30 18:52:37 +0000

    - forever is also a Qt keyword

--- trunk/src/cpphighlighter.cpp
+++ trunk/src/cpphighlighter.cpp
@@ -164,7 +164,7 @@ void CppHighlighter::setupRegexTable()
              << "volatile" << "wchar_t" << "while"
              // BK - Added Qt keywords
              << "slots" << "signals" << "SIGNAL" << "SLOT" << "connect" << "disconnect"
-             << "emit" << "qobject_cast";
+             << "emit" << "qobject_cast" << "forever";
 
    	//functions and methods
 	item.regex = QRegExp("\\b[a-zA-Z_][a-zA-Z0-9_]+\\s*(?=\\()");
